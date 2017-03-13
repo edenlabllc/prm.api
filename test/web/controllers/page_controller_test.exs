5ce450a4e53bda0e660cb47e60d11b533b4240bb
gmile <iamexile@gmail.com>
2017-03-13 17:59:18 +0200

    Fix tests

--- test/web/controllers/page_controller_test.exs
+++ test/web/controllers/page_controller_test.exs
@@ -2,7 +2,7 @@ defmodule PRM.Web.PageControllerTest do
   use PRM.Web.ConnCase
 
   test "GET /", %{conn: conn} do
-    conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    conn = get conn, "/api"
+    assert Map.get(json_response(conn, 200), "data")
   end
 end
