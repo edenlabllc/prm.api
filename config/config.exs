7b4e88b2bf9781d676463e99b5556d02e3b2b82b
Pavel Vesnin <paul@nebo15.com>
2017-03-13 17:28:10 +0200

    db config

--- config/config.exs
+++ config/config.exs
@@ -10,6 +10,16 @@ config :prm,
   ecto_repos: [PRM.Repo],
   namespace: PRM
 
+# Configure your database
+config :prm, PRM.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  username: "postgres",
+  password: "postgres",
+  database: "prm_api",
+  hostname: "localhost",
+  pool_size: 10
+
+
 # Configures the endpoint
 config :prm, PRM.Web.Endpoint,
   url: [host: "localhost"],
