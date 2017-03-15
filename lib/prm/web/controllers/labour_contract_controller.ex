60d9c7940825c7883d62bc23ab184dd99c89bb6e
gmile <iamexile@gmail.com>
2017-03-15 16:18:21 +0200

    Make dogma/credo happy

--- lib/prm/web/controllers/labour_contract_controller.ex
+++ lib/prm/web/controllers/labour_contract_controller.ex
@@ -14,7 +14,9 @@ defmodule PRM.Web.LabourContractController do
   end
 
   def create(conn, %{"labour_contract" => labour_contract_params}) do
-    with {:ok, %LabourContract{} = labour_contract} <- LabourContractAPI.create_labour_contract(labour_contract_params) do
+    result = LabourContractAPI.create_labour_contract(labour_contract_params)
+
+    with {:ok, %LabourContract{} = labour_contract} <- result do
       conn
       |> put_status(:created)
       |> put_resp_header("location", labour_contract_path(conn, :show, labour_contract))
@@ -29,7 +31,8 @@ defmodule PRM.Web.LabourContractController do
 
   def update(conn, %{"id" => id, "labour_contract" => labour_contract_params}) do
     labour_contract = LabourContractAPI.get_labour_contract!(id)
-    with {:ok, %LabourContract{} = labour_contract} <- LabourContractAPI.update_labour_contract(labour_contract, labour_contract_params) do
+    result = LabourContractAPI.update_labour_contract(labour_contract, labour_contract_params)
+    with {:ok, %LabourContract{} = labour_contract} <- result do
       render(conn, "show.json", labour_contract: labour_contract)
     end
   end
