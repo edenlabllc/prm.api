3ee710da383291803e3d6515253a19eba4bf52e3
Eugene Pirogov <iamexile@gmail.com>
2017-03-17 17:57:17 +0200

    Revise doctor schema (#41)
    * Convert some attrs into embed schemas
    
    * Add validate_required
    
    * Add some inclusion validations
    
    * license -> medical_license
    
    * Status validation for doctor

--- lib/prm/doctor_api/api.ex
+++ lib/prm/doctor_api/api.ex
@@ -38,9 +38,6 @@ defmodule PRM.DoctorAPI do
     fields = ~W(
       mpi_id
       status
-      education
-      certificates
-      licenses
       jobs
       active
       name
@@ -48,7 +45,116 @@ defmodule PRM.DoctorAPI do
       updated_by
     )
 
+    available_statuses = [
+      "PENDING_APPROVAL",
+      "APPROVED",
+      "DECLINED"
+    ]
+
     doctor
     |> cast(attrs, fields)
+    |> cast_embed(:education, with: &education_changeset/2)
+    |> cast_embed(:certificates, with: &certificate_changeset/2)
+    |> cast_embed(:licenses, with: &medical_license_changeset/2)
+    |> validate_inclusion(:status, available_statuses)
+  end
+
+  defp education_changeset(education, attrs) do
+    education_fields = ~W(
+      institution_name
+      certificate_number
+      degree
+      start_date
+      finished_date
+      speciality
+    )
+
+    required_fields = [
+      :institution_name,
+      :certificate_number,
+      :degree,
+      :start_date,
+      :finished_date,
+      :speciality
+    ]
+
+    available_degrees = [
+      "Doctor of Medicine",
+      "Bachelor of Medicine",
+      "Bachelor of Surgery",
+      "Doctor of Osteopathic Medicine",
+      "Master of Clinical Medicine"
+    ]
+
+    available_specialities = [
+      "Загальна практика - сімейна медицина",
+      "Педіатрія",
+      "Підліткова терапія",
+      "Терапія"
+    ]
+
+    education
+    |> cast(attrs, education_fields)
+    |> validate_required(required_fields)
+    |> validate_inclusion(:degree, available_degrees)
+    |> validate_inclusion(:speciality, available_specialities)
+  end
+
+  defp certificate_changeset(certificate, attrs) do
+    certificate_fields = ~W(
+      name
+      number
+      degree
+      issue_date
+      issued_by
+      start_date
+      finish_date
+    )
+
+    required_fields = [
+      :name,
+      :number,
+      :degree,
+      :issue_date,
+      :issued_by
+    ]
+
+    certificate
+    |> cast(attrs, certificate_fields)
+    |> validate_required(required_fields)
+  end
+
+  defp medical_license_changeset(licence, attrs) do
+    medical_license_fields = ~W(
+      category
+      type
+      issued_by
+      order_no
+      issued_date
+      expiry_date
+    )
+
+    required_fields = [
+      :category,
+      :type,
+      :issued_by,
+      :order_no,
+      :issued_date,
+      :expiry_date
+    ]
+
+    available_categories = [
+      "Атестація на визначення знань і практичних навиків"
+    ]
+
+    available_types = [
+      "Диплом"
+    ]
+
+    licence
+    |> cast(attrs, medical_license_fields)
+    |> validate_required(required_fields)
+    |> validate_inclusion(:category, available_categories)
+    |> validate_inclusion(:type, available_types)
   end
 end
