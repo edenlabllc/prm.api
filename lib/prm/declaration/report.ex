8120e3d0dc09dc2528cb0d35734d3af7ab28638e
gmile <iamexile@gmail.com>
2017-03-23 13:11:04 +0200

    Add working query in plain SQL

--- lib/prm/declaration/report.ex
+++ lib/prm/declaration/report.ex
@@ -4,6 +4,18 @@ defmodule PRM.Declaration.Report do
   alias PRM.Declaration
 
   def report(params) do
+    #       SELECT days.day,
+    #              count(declarations.id) as created,
+    #              count(case when status = 'closed' and DATE(updated_at) = day then 1 end) as closed,
+    #              count(case when status != 'closed' then 1 end) as total
+    #         FROM declarations
+    #   RIGHT JOIN (
+    #                SELECT date_trunc('day', days)::date AS day
+    #                  FROM generate_series ('2017-01-01'::timestamp, '2017-12-31'::timestamp, '1 day'::interval) days
+    #              ) days ON days.day = DATE(declarations.inserted_at)
+    #     GROUP BY days.day
+    #     ORDER BY days.day;
+
     query =
       from d in Declaration,
         where: d.doctor_id == ^params["doctor_id"],
