7df8f54ff29b5ee86c3f4e8d1804a97db5b81bca
gmile <iamexile@gmail.com>
2017-03-23 13:48:09 +0200

    Remove comments

--- lib/prm/web/controllers/report_controller.ex
+++ lib/prm/web/controllers/report_controller.ex
@@ -6,9 +6,5 @@ defmodule PRM.Web.ReportController do
   def declarations(conn, params) do
     declarations = PRM.Declaration.Report.report(params)
     render(conn, "declarations_report.json", declarations: declarations)
-    # + start_date: 2017-01-28 (string, required)
-    # + end_date: 2017-02-28 (string, required)
-    # + doctor_id: b075f148-7f93-4fc2-b2ec-2d81b19a9b7b (string, optional)
-    # + msp_id: b
   end
 end
